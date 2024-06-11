734753795a97d709ce2725320f636cec418cd5f7
Rafaelahy <rafaela.silva776@etec.sp.gov.br>
2024-06-11 08:08:06 -0300

    alterado taskList.js

--- src/screens/Task_List.js
+++ src/screens/Task_List.js
@@ -1,18 +1,25 @@
 import React, {Component} from "react" //import dos componentes
 import {StylesSheet, View, Text} from "react-native" //import do css, view e text
 
+import moment from "moment"
+import "moment/locale/pt-br"
+
 import today_Image from "../../assets/imgs/today.jpg"
 
 // cria uma classe padrão que será usada em outras partes do programa
 export default class TaskList extends Component{ // tem todas as características de um componente (herança)
     render(){
+        const today = moment().locale('pt-br').format('ddd, D [de] MMM')
         return(
             <View style ={styles.container}>
                 <ImageBackground source={today_Image} style={styles.background}>
-                    
+                    <view style={styles.titleBar}>
+                        <text style={styles.title}>Hoje</text>
+                        <text style={styles.subTitle}>{today}</text>
+                    </view>
                 </ImageBackground>
                 <View style={styles.taskList}>
-
+                        <text>Lista de tarefas</text>
                 </View>
             </View>
         )
@@ -29,6 +36,17 @@ const styles = StylesSheet.create({ //variável q guarda os estilos
     },
     taskList:{
         flex: 7
+    },
+    titleBar:{
+        flex: 1,
+        justifyContent: 'fkex-end'
+    },
+    title:{
+        fontFamily:'Arial',
+        fontSize: 50,
+        color: '#FFF',
+        marginLeft: 20,
+        marginBottom: 20
     }
 
 })
